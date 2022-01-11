6f576a2f1df927e795415f53f19d05f13b0a0abb
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-11 12:58:57 +0000

    Update dbModel.py

--- HorizonAPI/dbModel.py
+++ HorizonAPI/dbModel.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
+
 
 class vDesk(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +11,7 @@ class vDesk(db.Model):
     MachineStatus = db.Column(db.String(120), unique=False)
     AgentVersion = db.Column(db.Integer, unique=False)
     OSVersion = db.Column(db.String(120), unique=False)
+    user_id = db.Column(db.String(120), db.ForeignKey('UserName.id'), null=True)
 
     @property
     def serialize(self):
@@ -20,4 +23,19 @@ class vDesk(db.Model):
             'MachineStatus': self.MachineStatus,
             'AgentVersion': self.AgentVersion,
             'OSVersion': self.OSVersion
+        }
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    UserName = db.Column(db.String(120), unique=False)
+    Email = db.Column(db.String(140), unique=False)
+    MachineName = db.relationship('vDesk', backref='user_id', lazy=True)
+    @property
+    def serialize(self):
+        # Returns Data Object In Proper Format
+        return {
+            'UserName': self.UserName,
+            'Email': self.Email,
+            'MachineName': self.MachineName
         }
